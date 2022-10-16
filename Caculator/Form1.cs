d03a174a69aa1bea49510548e352c5eec5146e16
junkin3 <trannguyenan0104@gmail.com>
2022-10-16 14:40:10 +0700

    kiem tra textbox a va b

--- Caculator/Form1.cs
+++ Caculator/Form1.cs
@@ -19,10 +19,13 @@ namespace Caculator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double c = (int.Parse(txtA.Text) + int.Parse(txtB.Text));
+            if (txtA.Text != String.Empty && txtB.Text != String.Empty)
+            {
+                double c = (int.Parse(txtA.Text) + int.Parse(txtB.Text));
 
 
-            txtResult.Text = c.ToString();
+                txtResult.Text = c.ToString();
+            }
         }
 
         private void bttru_Click(object sender, EventArgs e)
