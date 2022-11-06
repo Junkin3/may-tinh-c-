5db765c4f370ef11dd21d3bfae246b15c11424e0
junkin3 <trannguyenan0104@gmail.com>
2022-11-06 07:52:04 +0700

    update test

--- TEST/UnitTest1.cs
+++ TEST/UnitTest1.cs
@@ -9,6 +9,7 @@ namespace TEST
     [TestClass]
     public class UnitTest1
     {
+        public TestContext TestContext { get; set; }
         private Calculation cal;
         [TestInitialize]
         public void SetUp()
@@ -41,6 +42,19 @@ namespace TEST
         {
             Calculation c = new Calculation(2, 0);
             c.Execute("/");
+        }
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+                    @".\Data\TestData.csv", "TestData#csv", DataAccessMethod.Sequential)]
+        public void TestWithDataSource()
+        {
+            int a, b, expected, actual;
+            a = int.Parse(TestContext.DataRow[0].ToString());
+            b = int.Parse(TestContext.DataRow[1].ToString());
+            expected = int.Parse(TestContext.DataRow[2].ToString());
+            cal = new Calculation(a, b);
+            actual = cal.Execute("+");
+            Assert.AreEqual(expected, actual);
         }
     }
 }
